cfc3ccd24e50bf873b8580c9a73f26b6b392a51e
Bubblesawn <3048311271@qq.com>
2025-08-22 08:53:48 +0800

    添加项目文件。

---
+++ titlebar.cpp
@@ -0,0 +1,235 @@
+#include "titlebar.h"
+#include <QHBoxLayout>
+#include <QPainter>
+#include <QPainterPath>
+#include <QRectF>
+#include <QWidget>
+#include <QFile>
+#include <QMouseEvent>
+
+
+#define BUTTON_HEIGHT 27
+#define BUTTON_WIDTH 27
+#define TITLE_HEIGHT 27
+
+
+
+
+TitleBar::TitleBar(QWidget *parent)
+        :QWidget(parent)
+        ,m_isPressed(false)
+        ,m_buttonType(MIN_MAX_BUTTON)
+{
+    initControl();
+    initConnections();
+    loadStyleSheet("Title");
+}
+
+TitleBar::~TitleBar()
+{
+
+}
+
+void TitleBar::initControl()
+{
+    m_pIcon = new QLabel(this);
+
+    m_pButtonMin = new QPushButton(this);
+	m_pButtonRestore = new QPushButton(this);
+	m_pButtonMax = new QPushButton(this);
+    m_pButtonClose = new QPushButton(this);
+
+    //按钮设置固定大小
+	m_pButtonMin->setFixedSize(QSize(BUTTON_WIDTH, BUTTON_HEIGHT));
+    m_pButtonRestore->setFixedSize(QSize(BUTTON_WIDTH, BUTTON_HEIGHT));
+	m_pButtonClose->setFixedSize(QSize(BUTTON_WIDTH, BUTTON_HEIGHT));
+    m_pButtonMax->setFixedSize(QSize(BUTTON_WIDTH, BUTTON_HEIGHT));
+
+    //设置对象名
+    m_pTitleContent->setObjectName("TitleContent");
+	m_pButtonMin->setObjectName("ButtonMin");
+	m_pButtonRestore->setObjectName("ButtonRestore");
+	m_pButtonMax->setObjectName("ButtonMax");
+    m_pButtonClose->setObjectName("ButtonClose");
+
+    //设置布局
+	QHBoxLayout* mylayout = new QHBoxLayout(this);
+    mylayout->addWidget(m_pIcon);
+	mylayout->addWidget(m_pTitleContent);
+    mylayout->addWidget(m_pButtonMin);
+	mylayout->addWidget(m_pButtonRestore);  
+    mylayout->addWidget(m_pButtonMax);
+    mylayout->addWidget(m_pButtonClose);
+    
+	mylayout->setContentsMargins(5, 0, 0, 0);  //设置布局边距
+    mylayout->setSpacing(0);                   //设置部件间的间隙
+    
+    m_pTitleContent->setSizePolicy(QSizePolicy::Expanding, QSizePolicy::Fixed);
+	setFixedHeight(TITLE_HEIGHT);              //设置标题栏高度
+	setWindowFlags(Qt::FramelessWindowHint);   //设置无边框窗口
+
+}
+
+//信号槽的连接
+void TitleBar::initConnections()
+{
+    connect(m_pButtonMin, SIGNAL(clicked), this, SLOT(onButtonMinClicked));
+    connect(m_pButtonRestore,SIGNAL(clicked), this, SLOT(onButtonRestoreCliced));
+    connect(m_pButtonMax, SIGNAL(clicked), this, SLOT(onButtonMaxClicked));
+    connect(m_pButtonClose, SIGNAL(clicked), this, SLOT(onButtonCloseClicked));
+}
+
+//设置标题栏图标
+void TitleBar::setTitleIcon(QString& filepath) {
+    QPixmap titleIcon(filepath);
+    m_pIcon->setFixedSize(titleIcon.size());
+    m_pIcon->setPixmap(titleIcon);
+}
+
+//设置标题栏内容
+void TitleBar::setTitleContent(QString& titlecontent){
+	m_pTitleContent->setText(titlecontent);
+	m_titleContent = titlecontent;//保存标题内容
+}
+
+//设置标题栏长度
+void TitleBar::setTitleWidth(int width) {
+    m_pTitleContent->setFixedWidth(width);
+}
+
+//设置按钮类型
+void TitleBar::setButtonType(ButtonType buttontype){
+	m_buttonType = buttontype;
+    switch (m_buttonType) {
+        case MIN_BUTTON:
+			m_pButtonRestore->setVisible(false);
+            m_pButtonMax->setVisible(false);
+            break;
+        case MIN_MAX_BUTTON:
+            m_pButtonRestore->setVisible(false);
+            break;
+        case ONLY_CLOSS_BUTTON:
+            m_pButtonMin->setVisible(false);
+            m_pButtonRestore->setVisible(false);
+            m_pButtonMax->setVisible(false);
+            break;
+		default:
+            break;
+	}
+
+}
+
+//保存当前窗口的位置信息和大小
+void TitleBar::saveResoreInfo(const QPoint &point, const QSize &size) {
+    m_restorePos = point;
+    m_restoreSize = size;
+}
+
+//获取当前窗口的位置信息和大小
+void TitleBar::getResoreInfo(QPoint& point, QSize& size) {
+    point = m_restorePos;
+    size = m_restoreSize;
+}
+
+//绘制标题栏
+void TitleBar::paintEvent(QPaintEvent* event) {
+    //设置背景色
+    QPainter painter(this);
+    QPainterPath pathBack;
+    pathBack.setFillRule(Qt::WindingFill); //设置填充规则
+    pathBack.addRoundedRect(QRect(0,0,width(),height()),3,3);//添加圆角矩形到绘图路径
+    painter.setRenderHint(QPainter::SmoothPixmapTransform, true);
+
+    //当窗口最大化或者还原后，窗口长度改变，标题栏相应做出改变
+    // parentWidget() 返回父部件
+    if (width() != parentWidget()->width()) {
+        setFixedWidth(parentWidget()->width());
+    }
+   
+}
+
+  //双击响应事件，主要实现双击标题栏进行最大化最小化的操作
+void TitleBar::mouseDoubleClickEvent(QMouseEvent* event) {
+    //只有存在最小化、最大化才有效
+    if (m_buttonType == MIN_MAX_BUTTON) {
+        if (m_pButtonMax->isVisible()) {
+            onButtonMaxClicked();
+        }
+        else {
+            onButtonRestoreCliced();
+        }
+    }
+    return QWidget::mouseDoubleClickEvent(event);
+}
+//通过鼠标按下、鼠标释放、鼠标移动事件实现标题栏达到移动窗口效果
+
+void TitleBar::mousePressEvent(QMouseEvent* event) {
+    QPointF posF = event->globalPosition();
+    if (m_buttonType == MIN_MAX_BUTTON) {
+        //在窗口最大化时禁止拖动
+        if (m_pButtonMax->isVisible()) {
+            m_isPressed = true;
+            m_startMovePos = posF.toPoint();  // 如果你只需要整数
+        }
+    }
+    else {
+        m_isPressed = true;
+        m_startMovePos = posF.toPoint();
+    }
+    return QWidget::mousePressEvent(event);
+}
+
+void TitleBar::mouseMoveEvent(QMouseEvent* event) {
+    QPointF posF = event->globalPosition();
+    if (m_isPressed) {
+        QPoint movePoint = posF.toPoint(); - m_startMovePos;
+        QPoint widgetPos = parentWidget()->pos();
+        m_startMovePos = posF.toPoint();
+        parentWidget()->move(widgetPos.x() + movePoint.x(), widgetPos.y() + movePoint.y());
+    }
+    
+    return QWidget::mouseMoveEvent(event);
+}
+
+void TitleBar::mouseReleaseEvent(QMouseEvent* event)
+{
+    m_isPressed = false;
+    return QWidget::mouseReleaseEvent(event);
+
+}
+
+//加载样式表
+
+void TitleBar::loadStyleSheet(const QString& sheetName) {
+
+    QFile file(":/Resources/QSS " + sheetName + ".css");
+    if (file.isOpen()) {
+        QString styleSheet = this->styleSheet();
+        styleSheet += QLatin1String(file.readAll());
+		setStyleSheet(styleSheet);
+
+     }
+
+}
+
+void TitleBar::onButtonMinClicked() {
+    emit signalButtonMinClicked();
+}
+
+
+void TitleBar::onButtonRestoreCliced() {
+    m_pButtonRestore->setVisible(false);
+    m_pButtonMax->setVisible(true);
+    emit signalButtonRestoreClicked();
+
+}
+
+void TitleBar::onButtonMaxClicked() {
+    m_pButtonRestore->setVisible(true);
+    m_pButtonMax->setVisible(false);
+    emit signalButtonMaxClicked();
+}
+
+void TitleBar::onButtonCloseClicked() {
+    emit signalButtonCloseClicked();
+}
---
+++ titlebar.h
@@ -0,0 +1,81 @@
+#pragma once
+
+#include <QWidget>
+#include <QLabel>
+#include <QPushButton>
+
+
+enum ButtonType
+{
+	MIN_BUTTON = 0,//最小化及关闭按钮
+	MIN_MAX_BUTTON,//最小化、最大化及关闭按钮
+	ONLY_CLOSS_BUTTON,//只有关闭按钮
+
+};
+
+//自定义标题栏
+
+class TitleBar : public QWidget
+{
+	Q_OBJECT
+
+public:
+	TitleBar(QWidget* parent);
+	~TitleBar();
+
+	void setTitleIcon(QString& filePath);         //设置标题栏图标
+	void setTitleContent(QString& title);         //设置标题内容
+	void setTitleWidth(int width);                //设置标题栏长度
+	void setButtonType(ButtonType buttonType);    //设置标题栏按钮类型
+
+
+	//保存、获取窗口最大化前窗口的位置和大小
+	void saveResoreInfo(const QPoint &point,const QSize &size);
+	void getResoreInfo(QPoint& point, QSize& size);
+
+private:
+		void paintEvent(QPaintEvent * event);             //绘图事件
+		void mouseDoubleClickEvent(QMouseEvent* event);  //鼠标双击事件
+		void mousePressEvent(QMouseEvent* event);        //鼠标按下事件
+		void mouseMoveEvent(QMouseEvent* event);         //鼠标移动事件
+		void mouseReleaseEvent(QMouseEvent* event);      //鼠标松开事件
+
+		void initControl();                              //初始化控件
+		void initConnections();                          //初始化信号和槽的连接
+		void loadStyleSheet(const QString &sheetname);   //加载样式表
+
+signals:
+	//按钮发射的信号
+	void signalButtonMinClicked();        //最小化按钮被点击
+	void signalButtonRestoreClicked();    //最大化还原按钮被点击
+	void signalButtonMaxClicked();        //最大化按钮被点击
+	void signalButtonCloseClicked();      //最大化关闭按钮被点击
+
+private slots:
+	//按钮响应的槽函数
+	void onButtonMinClicked();        //最小化按钮被点击
+	void onButtonRestoreCliced();     //最大化还原按钮被点击
+	void onButtonMaxClicked();        //最大化按钮被点击
+	void onButtonCloseClicked();      //关闭按钮被点击
+	  
+private:
+	QLabel* m_pIcon;               //标题栏图标
+    QLabel* m_pTitleContent;              //标题栏内容
+    QPushButton* m_pButtonMin;     //最小化按钮
+	QPushButton* m_pButtonMax;     //最大化按钮
+	QPushButton* m_pButtonRestore; //最大化还原按钮
+	QPushButton* m_pButtonClose;   //关闭按钮
+
+	//最大化还原按钮变量（用于保存窗体的位置及大小）
+	QPoint m_restorePos;          //最大化还原前的窗体位置
+	QSize m_restoreSize;        //最大化还原前的窗体大小
+
+	//移动窗口的变量
+	bool m_isPressed;            //鼠标是否按下;
+	QPoint m_startMovePos;       //鼠标按下时的坐标
+
+	QString m_titleContent;      //标题栏的内容
+	ButtonType m_buttonType;     //标题栏右上角按钮类型
+
+};
+
